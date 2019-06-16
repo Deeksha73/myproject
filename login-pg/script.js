b0e03b49de7e56d5d58e6ed6484cf2ce6e379f5a
Deeksha73 <deekshasingh945@gmail.com>
2019-06-16 23:13:43 +0530

    commit

--- login-pg/script.js
+++ login-pg/script.js
@@ -1,1 +1,12 @@
-var login = document.querySelector('')
+var register = document.querySelector('.registration-form');
+
+var login = document.querySelector('.register-link');
+
+login.addEventListener('click',function(){
+    document.querySelector('.log-form').style.display="none";
+    register.style.display="block";
+    document.querySelector('.general-form'){
+        
+    }
+    
+});
