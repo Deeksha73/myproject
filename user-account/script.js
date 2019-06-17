518bb7fa2bd28ea76a3165c5dc371c898f1e583a
Deeksha73 <deekshasingh945@gmail.com>
2019-06-17 21:23:07 +0530

    commit

--- user-account/script.js
+++ user-account/script.js
@@ -1,3 +1,5 @@
 document.querySelector('.setting-icon').addEventListener('click', function(){
     document.querySelector('.popup-setting').style.display="block";
+    document.querySelector('.popup-setting-menu').classList.add('popup-trans');
+    document.querySelector('.popup-setting-menu').classList.add('popup-trans-2');
 });
